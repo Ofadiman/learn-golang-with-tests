641b2c00666f96b31bdda442494c526c11514798
ofadiman <ofadiman@gmail.com>
2022-09-11 10:30:44 +0200

    refactor: create functions for assertions in tests

--- hello-world/say_hello_test.go
+++ hello-world/say_hello_test.go
@@ -2,6 +2,34 @@ package main
 
 import "testing"
 
+func assertEquals(t testing.TB, expected string, received string) {
+	t.Helper()
+	if expected != received {
+		t.Errorf("expected %v to equal %v", expected, received)
+	}
+}
+
+func assertNotEquals(t testing.TB, expected string, received string) {
+	t.Helper()
+	if expected == received {
+		t.Errorf("expected %v no to equal %v", expected, received)
+	}
+}
+
+func assertNil(t testing.TB, expected any) {
+	t.Helper()
+	if expected != nil {
+		t.Errorf("expected %v to equals nil", expected)
+	}
+}
+
+func assertNotNil(t testing.TB, expected any) {
+	t.Helper()
+	if expected == nil {
+		t.Errorf("expected %v not to equals nil", expected)
+	}
+}
+
 func TestSayHello(t *testing.T) {
 	const name = "Ofadiman"
 
@@ -9,42 +37,28 @@ func TestSayHello(t *testing.T) {
 		got, err := SayHello(name, EnglishLanguage)
 		want := EnglishHelloPrefix + name + HelloSuffix
 
-		if got != want {
-			t.Errorf("got %v, want %v", got, want)
-		}
-
-		if err != nil {
-			t.Errorf("function SayHello returned an error")
-		}
+		assertEquals(t, got, want)
+		assertNil(t, err)
 	})
 
 	t.Run("should return greeting in spanish when spanish language is passed", func(t *testing.T) {
 		got, err := SayHello(name, SpanishLanguage)
 		want := SpanishHelloPrefix + name + HelloSuffix
 
-		if got != want {
-			t.Errorf("got %v, want %v", got, want)
-		}
-
-		if err != nil {
-			t.Errorf("function SayHello returned an error")
-		}
+		assertEquals(t, got, want)
+		assertNil(t, err)
 	})
 
 	t.Run("should return error when name is empty", func(t *testing.T) {
 		_, err := SayHello("", EnglishLanguage)
 
-		if err == nil {
-			t.Errorf("function SayHello did not return error")
-		}
+		assertNotNil(t, err)
 	})
 
 	t.Run("should return error when language is not supported", func(t *testing.T) {
 		_, err := SayHello(name, "german")
 
-		if err == nil {
-			t.Errorf("function SayHello did not return error")
-		}
+		assertNotNil(t, err)
 	})
 
 }
